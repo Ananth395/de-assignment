47241c83bc315a400418aa587f8ff4df3d8af77e
ANANTHMURTHY RAO <ananthmurthy.rao@siemensgamesa.com>
2021-06-08 13:29:58 +0530

    change print message from branch4

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Laxmi Rao')
     print("Hello World")
+    print("My working code")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
