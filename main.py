00e2ba92e86ca400c4766da61b585cd3462a8348
ANANTHMURTHY RAO <ananthmurthy.rao@siemensgamesa.com>
2021-06-08 12:57:15 +0530

    change print message from branch3

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Ananth Rao')
+    print_hi('Laxmi Rao')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
