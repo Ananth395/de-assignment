edcd3350695cf5af43bd3929fffcccea01af1423
Ananth395 <35860580+Ananth395@users.noreply.github.com>
2021-06-08 16:05:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Laxmi Rao')
-    print("Hello World")
+    print("Hello World 123")
     print("My working code 103")
 
 
